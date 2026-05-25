e5123746c893036dee36ee35e79d846533a1aa71
lamtiaf <lamtiaf@mail.ru>
2026-05-25 20:04:00 +0400

    lab_4: add generate and verify

---
+++ lab_4/run_experiment.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+cd C:\Users\Varya\Desktop\lab_4
+
+for %%s in (200 400 800 1200 1600 2000) do (
+    echo ===== Размер %%s =====
+    python generate_matrices.py %%s
+    build\Release\lab_4.exe
+    echo.
+)
+
+echo ===== Верификация =====
+python verify.py
+pause
